475633083b32767beea5396cde3e2940ff344bdd
shyam10121998 <shyamrachchh77@gmail.com>
2024-02-25 16:50:57 -0800

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>FitFinder</title>
+</head>
+<body>
+
+<div class="header">
+  <img src="fitfinder-logo.png" alt="FitFinder Logo" class="logo">
+  <div class="nav">
+    <a href="#">Contact Us</a>
+    <a href="#">Stories</a>
+    <a href="#">Services</a>
+  </div>
+</div>
+
+</body>
+</html>
