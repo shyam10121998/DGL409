423e0786647bbfae3c381224944e8aaf954f09de
shyam10121998 <shyamrachchh77@gmail.com>
2024-03-19 01:25:39 -0700

    Add content section for  photography.php

--- parks.php
+++ parks.php
@@ -166,7 +166,13 @@ if ($result->num_rows > 0) {
 
   function toggleFree() {
     freeOnly = !freeOnly;
-    searchNearbyActivities('park');
+    const contentDiv = document.getElementById("content");
+        contentDiv.hidden = !event.target.checked;
+        if (event.target.checked) {
+            $("#content").show();
+        } else {
+            $("#content").hide();
+        }
   }
 </script>
 
