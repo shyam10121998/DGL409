dde56675a6f9e4fafb4125ec962d9405033641de
shyam10121998 <shyamrachchh77@gmail.com>
2024-03-19 00:35:43 -0700

    Update togglefree function to display free activities

--- bird-watching.php
+++ bird-watching.php
@@ -168,7 +168,13 @@ if ($result->num_rows > 0) {
 
   function toggleFree() {
     freeOnly = !freeOnly;
-    searchNearbyActivities('birdwatching');
+    const contentDiv = document.getElementById("content");
+      contentDiv.hidden = !event.target.checked;
+      if (event.target.checked) {
+        $("#content").show();
+      } else {
+        $("#content").hide();
+      }
   }
 </script>
 
