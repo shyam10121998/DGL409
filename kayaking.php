dc56fcafe767a45d2aedbfad78a49870d0589f08
shyam10121998 <shyamrachchh77@gmail.com>
2024-03-18 23:58:20 -0700

    Add php code to fetch the activity rows

--- kayaking.php
+++ kayaking.php
@@ -37,6 +37,30 @@ require_once './db_connection.php';
 <div class="map-container">
   <div id="map" style="height: 400px; width: 50%;"></div>
   <div id="place-details" class="results-container"></div>
+  <div id="content">
+
+                    <?php
+
+                    $sql = "SELECT * FROM `free_activities` WHERE `activity` = 'kayaking'";
+
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        // output data of each row
+                        while ($row = $result->fetch_assoc()) {
+                            echo '<p><strong>Name:</strong>' . $row['name'] . '</p>
+                          <p><strong>Address:</strong> <a href=' . $row['map_link'] . '>' . $row['address'] . '</a></p>
+                          <p><strong>Rating:</strong> ' . $row['rating'] . '</p>
+                          <p><strong>Open Now:</strong> ' . $row['open_now'] . '</p>
+                          <p><strong>Opening Hours:</strong><br> ' . $row['working_hours'] . '</p>
+                          <img src="images/' . $row['image'] . '" alt="Place Photo" style="max-width: 500px; height: 400px;">';
+                        }
+                    }
+                    for ($i = 0; $i <= 5; $i++) {
+                    }
+
+                    ?>
+                </div>
 </div>
 
 <script>
