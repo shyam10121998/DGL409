256857f8dd34d2601d2631f7f65825cbf8d01340
shyam10121998 <shyamrachchh77@gmail.com>
2024-03-18 22:01:32 -0700

    Add if else condition for login

--- login.php
+++ login.php
@@ -13,4 +13,26 @@ try {
     $alreadyExist = "SELECT * FROM `user` WHERE `email` = '" . $email . "'" . " and `password` = '" . md5($password) . "'";
 
     $result = $conn->query($alreadyExist);
+
+    if ($result->num_rows > 0) {
+
+        session_start();
+
+        while ($row = $result->fetch_assoc()) {
+            // echo $row['flag'];
+            // die;
+            if (isset($row['id'])) {
+
+                $_SESSION["fullName"] = $row['full_name'];
+
+                break;
+            } else {
+                die('Email or Password is Incorrect');
+            }
+        }
+    }
+
+    header('Location: stories.php');
+} catch (\Exception $e) {
+    die($e->getMessage());
 }
