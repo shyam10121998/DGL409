e021eeed60f556ef51c9626d817390b946f58061
shyam10121998 <shyamrachchh77@gmail.com>
2024-03-27 12:15:42 -0700

    Adjust styling for header and add logo

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!-- header-->
 <div class="header">
-<a href="services.php"><img src="#" alt="FitFinder Logo" class="logo"></a>
+<a href="services.php" class="logo">FitFinder</a>
     <div class="nav">
         <a href="contact.html">Contact Us</a>
         <a href="stories.php">Stories</a>
