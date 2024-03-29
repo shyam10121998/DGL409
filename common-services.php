b9783bebd9cf79339191f63e05c806eba234088f
shyam10121998 <shyamrachchh77@gmail.com>
2024-03-28 22:55:26 -0700

    Add more activities to the list

--- common-services.php
+++ common-services.php
@@ -14,6 +14,13 @@
     <link rel="stylesheet" href="style.css">
     <title><?php echo $activity ?> Activities</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "lnxfe0v3nr");
+</script>
 </head>
 
 <body>
